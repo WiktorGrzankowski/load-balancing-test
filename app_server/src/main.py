86c8c1a15e181542407282abd30fb653d24a05c1
Ubuntu <st124@st124vm101.rtb-lab.pl>
2024-08-24 19:14:24 +0000

    another endpoint

--- app_server/src/main.py
+++ app_server/src/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 async def root():
     print("i got called!")
     return {"message": "Hello World"}
+
+
+@app.get("/endpoint")
+async def endpoint():
+    print("endpoint called")
+    return {"message": "endpoint"}
